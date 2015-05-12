46b09ac8ff7d5099564def302bc3fedd6068a39e
Jason L. Walker <jasonlanewalker@gmail.com>
2015-05-12 10:25:04 -0500

    added stubs for Uri and Request classes including tests

---
+++ test/RequestTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Unit test for \Fusion\Http\Request class.
+ */
+
+namespace Fusion\Http\Test;
+
+
+class RequestTest
+{
+
+}
---
+++ test/UriTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Unit test for \Fusion\Http\Uri class.
+ */
+namespace Fusion\Http\Test;
+
+
+class UriTest
+{
+
+}
