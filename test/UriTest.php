0b6f7711c48c93c3dfa134d849d166431a01b67d
Jason L. Walker <jasonlanewalker@gmail.com>
2015-05-15 15:51:29 -0500

    added more tests to UriTest

--- test/UriTest.php
+++ test/UriTest.php
@@ -10,9 +10,12 @@ require '../vendor/autoload.php';
 
 class UriTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Fusion\Http\Uri
+     */
     private $uri = null;
 
-    private $testUrl = 'http://billybob:foobar@example.org/resource/target?query=blah#fragmerunning';
+    private $testUrl = 'http://billybob:foobar@example.org:8080/resource/target?query=blah#fragmerunning';
 
     public function setUp()
     {
@@ -24,5 +27,43 @@ class UriTest extends \PHPUnit_Framework_TestCase
         $this->uri = null;
     }
 
+    public function testGettingScheme()
+    {
+        $this->assertEquals('http', $this->uri->getScheme());
+    }
 
+    public function testGettingAuthority()
+    {
+        $this->assertEquals('billybob:foobar@example.org', $this->uri->getAuthority());
+    }
+
+    public function testGettingUserInfo()
+    {
+        $this->assertEquals('billybob:foobar', $this->uri->getUserInfo());
+    }
+
+    public function testGettingHost()
+    {
+        $this->assertEquals('example.org', $this->uri->getHost());
+    }
+
+    public function testGettingPort()
+    {
+        $this->assertEquals(8080, $this->uri->getPort());
+    }
+
+    public function testGettingPath()
+    {
+        $this->assertEquals('/resource/target', $this->uri->getPath());
+    }
+
+    public function testGettingQuery()
+    {
+        $this->assertEquals('query=blah', $this->uri->getQuery());
+    }
+
+    public function testGettingFragment()
+    {
+        $this->assertEquals('fragmerunning', $this->uri->getFragment());
+    }
 }
