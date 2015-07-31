a86ca1402fc2bdb3ccf262714b7f1e617664275d
Jason L. Walker <jasonlanewalker@gmail.com>
2015-07-31 15:56:01 -0500

    started unit test (RequestTest) for Request class

--- test/RequestTest.php
+++ test/RequestTest.php
@@ -5,8 +5,30 @@
 
 namespace Fusion\Http\Test;
 
+use Fusion\Http\Request;
 
-class RequestTest
+require '../vendor/autoload.php';
+
+class RequestTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Fusion\Http\Request
+     */
+    private $request;
+
+    public function setUp()
+    {
+        $this->request = new Request(
+            'GET',
+            'http://www.example.com',
+            ['X-Test-Header' => 'foobar', 'Content-Type' => 'text/plain'],
+            $this->getMock('Psr\Http\Message\StreamInterface')
+        );
+    }
+
+    public function testGetEmptyRequestTarget()
+    {
+        $this->assertEquals('/', $this->request->getRequestTarget());
+    }
 
 }
